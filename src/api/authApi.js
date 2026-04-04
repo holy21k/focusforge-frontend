3d9e943e8dedae6b25041b375cbf2742a1ba3b29
holy21k <kidistteshome@72gmail.com>
2026-04-04 17:27:40 +0300

    feat(auth-api): update authentication API logic

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -4,7 +4,7 @@ const authApi = {
   login: (data) => axiosClient.post('auth/login', data),
   register: (data) => axiosClient.post('auth/register', data),
   getMe: () => axiosClient.get('auth/me'),
-  googleLogin: (token) => axiosClient.post('auth/google', { token }),
+  googleLogin: (token, redirectUri) => axiosClient.post('auth/google', { token, redirect_uri: redirectUri }),
 };
 
 export default authApi;
